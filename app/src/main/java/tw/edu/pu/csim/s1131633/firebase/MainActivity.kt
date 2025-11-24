e718aadb6d519aa6bc2f24bf20d1b59becfd04bf
chang200601-ubs <chang200601@gmail.com>
2025-11-24 15:43:58 +0800

    v.2新增資料

--- app/src/main/java/tw/edu/pu/csim/s1131633/firebase/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1131633/firebase/MainActivity.kt
@@ -19,29 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FirebaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                UserScoreScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirebaseTheme {
-        Greeting("Android")
     }
 }
